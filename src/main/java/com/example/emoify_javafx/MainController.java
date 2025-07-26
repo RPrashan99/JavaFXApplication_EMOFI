82711bb950d6fda5c42e71efb31d07f671509a2f
RPrashan99 <RashmithaMK@gmail.com>
2025-07-26 19:25:20 +0530

    major - APP sub window creation

--- src/main/java/com/example/emoify_javafx/MainController.java
+++ src/main/java/com/example/emoify_javafx/MainController.java
@@ -1,6 +1,7 @@
 package com.example.emoify_javafx;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
@@ -12,7 +13,7 @@ import javafx.stage.Stage;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class MainController {
+public class MainController implements Initializable {
     @FXML
     private Button appsButton;
 
@@ -34,4 +35,37 @@ public class MainController {
         stage.close();
     }
 
+    @FXML
+    void setContent(String fxmlPath){
+        try {
+            AnchorPane newContent = FXMLLoader.load(getClass().getResource(fxmlPath));
+            contentPane.getChildren().setAll(newContent);
+            AnchorPane.setTopAnchor(newContent, 0.0);
+            AnchorPane.setBottomAnchor(newContent, 0.0);
+            AnchorPane.setLeftAnchor(newContent, 0.0);
+            AnchorPane.setRightAnchor(newContent, 0.0);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void loadHomeWindow() {
+        setContent("fxmls/homeSubWindow.fxml");
+    }
+
+    @FXML
+    private void loadAppsWindow() {
+        setContent("fxmls/exAppWindow.fxml");
+    }
+
+//    @FXML
+//    private void loadSettingsWindow() {
+//        setContent("Settings.fxml");
+//    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        loadHomeWindow();
+    }
 }
